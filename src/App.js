0bdc0931bfc0c77449fe27f8d248bf544533d92f
elenaBushueva <helenabushueva@gmail.com>
2021-09-15 23:37:06 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const initialList = [
+    {id: 1, name: 'Learn JS'},
+    {id: 2, name: 'Learn React'}
+]
+
+const App = () => {
+
+    const [list, setList] = useState(initialList);
+    const [input, setInput] = useState('');
+
+    const onChangeInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const addToList = () => {
+        setList([...list, {id: Math.random(), name: input}])
+        setInput('');
+    }
+
+    return (
+        <div>
+
+            <input type="text" value={input} onChange={onChangeInput}/>
+            <button onClick={addToList}>Add to list</button>
+
+            <ul>
+                {list.map(el => <li key={el.id}>{el.name}</li>)}
+            </ul>
+        </div>
+    );
+};
 
 export default App;
